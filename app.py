01d0b42840e999e2f38216c8c0764709086ba3e1
Falcao Zane Vijay <112317470+falcaozane@users.noreply.github.com>
2023-09-07 19:13:42 +0530

    Initial

---
+++ app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import fitz
+import spacy
+import pandas as pd
+
+# Load the SpaCy NER model
+nlp = spacy.load('./models/model-best')
+
+# Streamlit app title and description
+st.title("Resume Parser")
+st.write("Upload a PDF containing a resume for parsing.")
+
+# File upload widget
+uploaded_file = st.file_uploader("Upload a PDF", type=["pdf"])
+
+if uploaded_file is not None:
+    # Read the PDF file
+    pdf_data = uploaded_file.read()
+    
+    # Extract text from the PDF using PyMuPDF (fitz)
+    doc = fitz.open("uploaded.pdf", pdf_data)
+    text = " ".join([page.get_text() for page in doc])
+    
+    # Process the text with the SpaCy NER model
+    parsed_doc = nlp(text)
+    
+    # Display the extracted entities
+    st.header("Extracted Entities:")
+    for ent in parsed_doc.ents:
+        st.write(f"{ent.label_} : {ent.text}")
+
+    st.divider()
+
+    # Create a list of entities and labels
+    entities = [ent.text for ent in parsed_doc.ents]
+    labels = [ent.label_ for ent in parsed_doc.ents]
+    
+    # Create a Pandas DataFrame
+    data = {'Label': labels, 'Content': entities}
+    df = pd.DataFrame(data)
+    
+    # Display the extracted entities in a table
+    st.header("Extracted Entities in Tabular format:")
+    st.dataframe(df)
